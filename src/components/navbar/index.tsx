2c435a9203fc26a987cd42e4c98749f5ba439951
Ww <nickolaswewe2@gmail.com>
2025-02-02 12:32:50 +0700

    feat: folder structure

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { SlBasket } from "react-icons/sl";
+import { MdFavoriteBorder } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='mt-5 '>
+      {/* <h1 className=''>Ww's Market</h1> */}
+      <div className='flex gap-10 justify-center items-center'> 
+          <p>Home</p>
+          <p>Contact</p>
+          <p>About</p>
+          <p>Sign Up</p>
+        {/* <div className='flex gap-10'> 
+          <MdFavoriteBorder />
+          <SlBasket />
+        </div> */}
+      </div>
     </div>
   )
 }
