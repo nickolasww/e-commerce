ca41692250ae88e4fdf65633066f7106403dfbca
your_github_username <your_github_email>
2025-02-08 23:08:44 +0700

    feat: register page 50 persen

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Input = () => {
+const Input = ({type, placeholder}) => {
   return (
     <div>
-      
+      <input
+        type  = {type}
+        placeholder = {placeholder}
+        className=" border-b-2 border-gray-300 w-72 focus:outline-none"
+        />
     </div>
   )
 }
--- src/pages/register/partials/index.tsx
+++ src/pages/register/partials/index.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 import Navbar from "../../../components/navbar";
+import Input from "../../../components/input";
 
 const Register = () => {
   return (
     <header className="">
       <Navbar />
-      <div className="w-1/2">
-        <img src="" alt="" className="" />
-      </div>
-      <div className="w-1/2">
-        <h1>Create an account</h1>
-        <p>Enter your details below</p>
+      <div className="flex justify-center items-center">
+        <div className="w-">
+          <img src="" alt="" className="" />
+        </div>
 
-        <input
-        type="text" 
-        placeholder="Name"
-        className="border-b-2 border-gray-500 focus:outline-none"
-        />
+        <div className="">
+          <h1 className="text-5xl font-normal">Create an account</h1>
+          <p className="mt-2">Enter your details below</p>
 
-        <button className="text-white bg-red-600 hover:bg-red-700 px-20 py-3">
-          Create Account
-        </button>
+          <div className="flex flex-col gap-8 my-5">
+            <Input type="text" placeholder="Name" />
 
-        <p className="flex gap-3">
-          Already have account?
-          <a href="" className="font-semibold">
-            Log in
-          </a>
-        </p>
+            <Input type="text" placeholder="Email or Phone Number" />
+
+            <Input type="text" placeholder="Password" />
+          </div>
+
+          <button className="text-white bg-red-600 hover:bg-red-700 px-20 py-3">
+            Create Account
+          </button>
+
+          <p className="flex gap-3">
+            Already have account?
+            <a href="" className="font-semibold">
+              Log in
+            </a>
+          </p>
+        </div>
       </div>
     </header>
   );
