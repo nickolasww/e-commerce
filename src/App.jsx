39a04ece735d08ef286eac7757c3b6c88b8a4846
Ww <nickolaswewe2@gmail.com>
2025-02-20 09:40:26 +0700

    feat: Stucture About us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from './pages/Home/partials/index'
 import Register from './pages/Register/partials/index'
 import Login from './pages/Login/partials/index'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './pages/About/partials/index'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage/>} />
+                <Route path="/About" element={<About/>} />
                 <Route path="/register" element={<Register/>} />
                 <Route path="/login" element={<Login/>} />
             </Routes>
--- src/pages/about/partials/index.tsx
+++ src/pages/about/partials/index.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
-
-
+import Navbar from "../../../components/navbar";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+      <header>
+        <Navbar />
+        <div>
+          <div className="w-1/2">
+            <h1> Our Story</h1>
+            <p> Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense
+              in Bangladesh. Supported by wide range of tailored marketing, data and service solutions,
+              Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region.
+            </p>
+            <p>
+              Exclusive has more than 1 Million products to offer,
+              growing at a very fast. Exclusive offers a diverse
+              assotment in categories ranging  from consumer.
+            </p>
+          </div>
+
+          <div className="w-1/2">
+            <img  src="" alt="" />
+          </div>
+        </div>
+      </header>
   )
 }
 
